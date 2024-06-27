2f7d94c30e7afd77048af3204c4229ef3e910e52
Sther Marinho <smbrito@sga.pucminas.br>
2024-06-27 08:40:36 -0300

    arrumando a rota

--- assets/js/jsonserver.js
+++ assets/js/jsonserver.js
@@ -1,4 +1,5 @@
 // globals.js
 (function(window) {
-    window.apiUrl = window.apiUrl || "http://localhost:3000/";
+    // window.apiUrl = window.apiUrl || "http://localhost:3000/";
+    window.apiUrl = window.apiUrl || "https://journey-server-rwem.onrender.com/";
 })(window);
